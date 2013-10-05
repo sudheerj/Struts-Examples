875fe67f4abb59cb789a9ddbce024f725a192f9b
sudheerj <sudheer.jonna@gmail.com>
2013-10-06 01:02:38 +0530

    HelloWorld ResultPath

---
+++ Struts2/Overview-Basics/HelloWorld-AnnotationVersion/src/main/java/com/sudheer/user/common/LoginAction.java
@@ -0,0 +1,14 @@
+package com.sudheer.user.action;
+
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.ResultPath;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@Namespace("/User")
+@ResultPath(value="/")
+@Result(name="success",location="pages/login.jsp")
+public class LoginAction extends ActionSupport{
+
+}
---
+++ Struts2/Overview-Basics/HelloWorld-ResultPath/src/main/java/com/sudheer/user/action/WelcomeUserAction.java
@@ -0,0 +1,32 @@
+package com.sudheer.user.action;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.ResultPath;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@Namespace("/User")
+@ResultPath(value="/")// Defaults to WEB-INF/content/
+public class WelcomeUserAction extends ActionSupport{
+
+	private String username;
+	 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Action(value="Welcome", results={
+			@Result(name="success",location="pages/welcome_user.jsp")
+	})
+	public String execute() {
+
+		return SUCCESS;
+
+	}
+}
